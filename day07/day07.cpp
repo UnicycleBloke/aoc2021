c1c9e474d8a52457b47505a0ab53a45ca3271351
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-06 22:23:29 +0000

    Prepping for Day 7

---
+++ day07/day07.cpp
@@ -0,0 +1,55 @@
+#include "utils.h"
+
+
+template <typename T>
+auto part1(const T& input)
+{
+    return 0;
+}
+
+
+template <typename T>
+auto part2(const T& input)
+{
+    return 0;
+}
+
+
+void run(const char* filename)
+{
+    auto input = aoc::read_lines<uint32_t, uint32_t, uint32_t, uint32_t>(filename, R"((\d+),(\d+)\s->\s(\d+),(\d+))");
+
+    auto p1 = part1(input);
+    cout << "Part1: " << p1 << '\n';
+    //aoc::check_result(p1, 6687);
+
+    auto p2 = part2(input);
+    cout << "Part2: " << p2 << '\n';
+    //aoc::check_result(p2, 19851);
+}
+
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        if (argc < 2)
+        {
+            cout << "Provide input file name\n";
+            return -1;
+        }
+
+        fs::path path{argv[1]};
+        if (!fs::is_regular_file(path))
+        {
+            cout << "Path '" << argv[1] << "' does not exist or is not a file\n";
+            return -1;
+        }
+
+        run(argv[1]);
+    }
+    catch (std::exception& e)
+    {
+        cout << e.what();
+    }
+}
