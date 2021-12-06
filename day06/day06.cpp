1a6fdcbb7fefb3f6196d4901825a2b2a619d082c
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-06 06:36:06 +0000

    Used part 2 solution for part 1 because it is just so much better.

--- day06/day06.cpp
+++ day06/day06.cpp
@@ -1,34 +1,7 @@
 #include "utils.h"
 
 
-int part1(vector<int> input)
-{
-    for (int c = 0; c < 80; ++c)
-    {
-        vector<int> temp;
-        temp.reserve(1000000);
-        for (int f: input)
-        {
-            if (f > 0)
-            {
-                temp.push_back(f - 1);
-            }
-            else
-            {
-                temp.push_back(6);
-                temp.push_back(8);
-            }
-        }
-
-        input = temp;
-        cout << " " << input.size();
-    }
-
-    return input.size();
-}
-
-
-uint64_t part2(vector<int> input)
+uint64_t reproduce(vector<int> input, int days)
 {
     array<uint64_t, 10> n{};
 
@@ -38,7 +11,7 @@ uint64_t part2(vector<int> input)
         ++n[(f + 1)];
     }
 
-    for (int d = 0; d < 257; ++d)
+    for (int d = 0; d <= days; ++d)
     {
         auto temp = n[0]; 
         total += temp;
@@ -58,6 +31,18 @@ uint64_t part2(vector<int> input)
 }
 
 
+auto part1(const vector<int>& input)
+{
+    return reproduce(input, 80);
+}
+
+
+auto part2(const vector<int>& input)
+{
+    return reproduce(input, 256);
+}
+
+
 void run(const char* filename)
 {
     ifstream is{filename};
@@ -70,13 +55,13 @@ void run(const char* filename)
         input.push_back(i);
     }
 
-    //auto p1 = part1(input);
-    //cout << "Part1: " << p1 << '\n';
-    //aoc::check_result(p1, 0);
+    auto p1 = part1(input);
+    cout << "Part1: " << p1 << '\n';
+    aoc::check_result(p1, 390011ull);
 
     auto p2 = part2(input);
     cout << "Part2: " << p2 << '\n';
-    //aoc::check_result(p2, 0);
+    aoc::check_result(p2, 1746710169834ull);
 }
 
 
