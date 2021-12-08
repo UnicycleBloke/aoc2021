c2f0fa2a633417ed923a5d3b86977c6ed188801e
Alan Chambers <alan.chambers@cambridgeconsultants.com>
2021-12-08 16:34:27 +0000

    Prepping for Day 9

---
+++ day09/day09.cpp
@@ -0,0 +1,43 @@
+#include "utils.h"
+
+
+template <typename T>
+auto part1(const T& input)
+{
+    auto fuel = numeric_limits<typename T::value_type>::max();
+    for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
+        fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
+        {
+            return sum + abs(i - j);
+        }));
+    return fuel;
+}
+
+
+template <typename T>
+auto part2(const T& input)
+{
+    auto fuel = numeric_limits<typename T::value_type>::max();
+    for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
+        fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
+        {
+            auto f = abs(i - j);
+            return sum + f * (f + 1) / 2;
+        }));
+    return fuel;
+}
+
+
+void run(const char* filename)
+{
+    auto lines = aoc::read_lines(filename);
+    auto input = aoc::make_vector<int>(lines[0]);
+
+    auto p1 = part1(input);
+    cout << "Part1: " << p1 << '\n';
+    aoc::check_result(p1, 340052);
+
+    auto p2 = part2(input);
+    cout << "Part2: " << p2 << '\n';
+    aoc::check_result(p2, 92948968);
+}
