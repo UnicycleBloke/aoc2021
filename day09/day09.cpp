5fb3b2ec88dd95dd47156d81d668a209c554398c
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-08 21:20:33 +0000

    Prepping for Day 9 at home

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -7,8 +7,8 @@ auto part1(const T& input)
     auto fuel = numeric_limits<typename T::value_type>::max();
     for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
         fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
-        {
-            return sum + abs(i - j);
+        { 
+            return sum + abs(i - j); 
         }));
     return fuel;
 }
@@ -20,9 +20,9 @@ auto part2(const T& input)
     auto fuel = numeric_limits<typename T::value_type>::max();
     for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
         fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
-        {
-            auto f = abs(i - j);
-            return sum + f * (f + 1) / 2;
+        { 
+            auto f = abs(i - j); 
+            return sum + f * (f + 1) / 2; 
         }));
     return fuel;
 }
@@ -40,4 +40,30 @@ void run(const char* filename)
     auto p2 = part2(input);
     cout << "Part2: " << p2 << '\n';
     aoc::check_result(p2, 92948968);
+}
+
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        if (argc < 2)
+        {
+            cout << "Provide input file name\n";
+            return -1;
+        }
+
+        fs::path path{argv[1]};
+        if (!fs::is_regular_file(path))
+        {
+            cout << "Path '" << argv[1] << "' does not exist or is not a file\n";
+            return -1;
+        }
+
+        run(argv[1]);
+    }
+    catch (std::exception& e)
+    {
+        cout << e.what();
+    }
 }
