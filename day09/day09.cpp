3c0ef83cfbc7e81589dfb213cbc56681e123a691
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-09 06:31:39 +0000

    Day 9 solution.

--- day09/day09.cpp
+++ day09/day09.cpp
@@ -4,42 +4,123 @@
 template <typename T>
 auto part1(const T& input)
 {
-    auto fuel = numeric_limits<typename T::value_type>::max();
-    for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
-        fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
-        { 
-            return sum + abs(i - j); 
-        }));
-    return fuel;
+    int total_risk = 0;
+    for (int i = 1; i <= 100; ++i)
+    {
+        for (int j = 1; j <= 100; ++j)
+        {
+            int  height = input[i][j];
+            bool low = true;
+            low = low && (height < input[i+1][j]);
+            low = low && (height < input[i-1][j]);
+            low = low && (height < input[i][j+1]);
+            low = low && (height < input[i][j-1]);
+
+            if (low)
+            {
+                total_risk += height + 1;
+            }
+        }
+    }
+
+    return total_risk;
+}
+
+
+// Recursively explore the basin
+template <typename T>
+int basin_size_impl(T& input, int i, int j)
+{
+    if (input[i][j] < 9)
+    {        
+        input[i][j] = 10;
+
+        int size = 1;
+        size += basin_size_impl(input, i+1, j);
+        size += basin_size_impl(input, i-1, j);
+        size += basin_size_impl(input, i, j+1);
+        size += basin_size_impl(input, i, j-1);
+        return size;
+    }
+    return 0;
 }
 
 
 template <typename T>
-auto part2(const T& input)
+auto basin_size(T& input, int i, int j)
 {
-    auto fuel = numeric_limits<typename T::value_type>::max();
-    for (auto i = 0; i < *max_element(input.begin(), input.end()); ++i)
-        fuel = min(fuel, accumulate(input.begin(), input.end(), 0, [i](auto sum, auto j)
-        { 
-            auto f = abs(i - j); 
-            return sum + f * (f + 1) / 2; 
-        }));
-    return fuel;
+    // Already counted this cell
+    if (input[i][j] == 10) return 0;
+    // Not in any basin
+    if (input[i][j] == 9) return 0;
+
+    int  height = input[i][j];
+    bool low = true;
+    low = low && (height < input[i+1][j]);
+    low = low && (height < input[i-1][j]);
+    low = low && (height < input[i][j+1]);
+    low = low && (height < input[i][j-1]);
+
+    if (low)
+    {        
+        return basin_size_impl(input, i, j);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+template <typename T>
+auto part2(T& input)
+{
+    vector<int> sizes;
+    for (int i = 1; i <= 101; ++i)
+    {
+        for (int j = 1; j <= 101; ++j)
+        {            
+            int size = basin_size(input, i, j);
+            sizes.push_back(size);
+        }
+    }
+
+    sort(sizes.begin(), sizes.end());
+    reverse(sizes.begin(), sizes.end());
+
+    return sizes[0] * sizes[1] * sizes[2];
 }
 
 
 void run(const char* filename)
 {
     auto lines = aoc::read_lines(filename);
-    auto input = aoc::make_vector<int>(lines[0]);
+    array<array<int, 102>, 102> input{};
+
+    // Use a grid with a border of 9s to obviate complicated boundary tests.
+    for (int i = 0; i <= 101; ++i)
+    {
+        for (int j = 0; j <= 101; ++j)
+        {
+            input[i][j] = 9;
+        }
+    }
+
+    for (int i = 1; i <= lines.size(); ++i)
+    {
+        for (int j = 1; j <= lines[0].size(); ++j)
+        {
+            input[i][j] = lines[i-1][j-1] - '0';
+        }
+    }
 
     auto p1 = part1(input);
     cout << "Part1: " << p1 << '\n';
-    aoc::check_result(p1, 340052);
+    aoc::check_result(p1, 494);
 
     auto p2 = part2(input);
     cout << "Part2: " << p2 << '\n';
-    aoc::check_result(p2, 92948968);
+    aoc::check_result(p2, 1048128);
 }
 
 
