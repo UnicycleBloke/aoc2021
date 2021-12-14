5912e3103bdbd56ba81ea5cd259debaa3ef36d45
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-14 07:48:57 +0000

    Added some comments for future reference.

--- day14/day14.cpp
+++ day14/day14.cpp
@@ -6,6 +6,8 @@ using State = map<Pair, uint64_t>;
 using Rules = map<Pair, pair<Pair, Pair>>;
 
 
+// It seems I could just directly manipulate a frequency counting table rather 
+// than the indirection I have here. 
 auto grow(State src, Rules& rules)
 {
     State dst;
@@ -34,11 +36,14 @@ auto solve(State init, Pair ends, Rules& rules, int steps)
     uint64_t cmax =  0;
     uint64_t cmin = -1;
 
+    // Grow the numbers of pairs of characters in use.
     for (int i: aoc::range(steps))
-    {
         init = grow(init, rules);
-    }
 
+    // Extract the numbers of each pair and sum for each 
+    // character. This results in double counting. We have 
+    // to adjust for the ends, which are not double counted
+    // in the loop.
     map<char, uint64_t> counts;
     counts[ends.first] = 1;
     counts[ends.second] = (ends.second == ends.first) ? 2 : 1;
@@ -55,6 +60,8 @@ auto solve(State init, Pair ends, Rules& rules, int steps)
         counts[B] += v;
     }
 
+    // Extract the min and max. A map was easier than trying to 
+    // browbeat min_element to ignore zeroes.
     for (auto [k, v]: counts)
     {
         v = v / 2;
