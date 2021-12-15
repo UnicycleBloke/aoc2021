5e963288100270d3fe7fccbc8090115050f0cebe
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-15 16:56:43 +0000

    Finally got Dijkstra working for Part 1. Is it too slow for Part 2?

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -1,16 +1,91 @@
 #include "utils.h"
 
 
-template <typename T, typename U>
-auto part1(T points, U folds)
+template <typename T>
+auto part1(T input)
 {
     aoc::timer timer;
-    return 0;
+
+    const uint32_t COLS = static_cast<uint32_t>(input[0].size());
+    const uint32_t ROWS = static_cast<uint32_t>(input.size());
+
+    using Pos = pair<uint32_t, uint32_t>;
+
+    // 1. Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
+    set<Pos> unvisited;
+    for (auto r: aoc::range{ROWS})
+        for (auto c: aoc::range{COLS})
+            unvisited.insert({r, c});
+
+    // 2. Assign to every node a tentative distance value: set it to zero for our initial node 
+    // and to infinity for all other nodes. The tentative distance of a node v is the length of 
+    // the shortest path discovered so far between the node v and the starting node. Since initially 
+    // no path is known to any other vertex than the source itself (which is a path of length zero), 
+    // all other tentative distances are initially set to infinity. Set the initial node as current.
+    vector<uint32_t> distance_row(COLS, 0xFFFF'FFFF); // Infinity
+    vector<vector<uint32_t>> distance(ROWS, distance_row);
+    distance[0][0] = 0;
+    Pos current = {0, 0};
+
+    while (unvisited.size() > 0)
+    {
+        // 3. For the current node, consider all of its unvisited neighbors and calculate their tentative 
+        // distances through the current node. Compare the newly calculated tentative distance to the current 
+        // assigned value and assign the smaller one. For example, if the current node A is marked with a 
+        // distance of 6, and the edge connecting it with a neighbor B has length 2, then the distance to B 
+        // through A will be 6 + 2 = 8. If B was previously marked with a distance greater than 8 then change 
+        // it to 8. Otherwise, the current value will be kept.
+        auto [x, y] = current;
+
+        // All neighbours
+        vector<Pos> neighbours;
+        if (x < (COLS-1)) neighbours.push_back({x + 1, y});
+        if (y < (ROWS-1)) neighbours.push_back({x, y + 1});
+        if (x > 0)        neighbours.push_back({x - 1, y});
+        if (y > 0)        neighbours.push_back({x, y - 1});
+
+        for (auto pos: neighbours)
+        {
+            // Ignore neighbours already visited
+            if (unvisited.find(pos) == unvisited.end())
+                continue;
+
+            // Maybe reduce the tentative distance
+            auto [i, j] = pos;    
+            distance[i][j] = min(distance[i][j], distance[x][y] + input[i][j]);
+        }
+
+        // 4. When we are done considering all of the unvisited neighbors of the current node, mark the 
+        // current node as visited and remove it from the unvisited set. A visited node will never be 
+        // checked again.
+        unvisited.erase(current);
+
+        // 5. If the destination node has been marked visited (when planning a route between two specific nodes) 
+        // or if the smallest tentative distance among the nodes in the unvisited set is infinity (when planning 
+        // a complete traversal; occurs when there is no connection between the initial node and remaining unvisited 
+        // nodes), then stop. The algorithm has finished.
+        if (current == Pos{ ROWS-1, COLS-1})
+            return distance[ROWS-1][COLS-1];
+
+        // 6. Otherwise, select the unvisited node that is marked with the smallest tentative distance, 
+        // set it as the new current node, and go back to step 3.
+        uint32_t min_distance = 0xFFFF'FFFF;
+        for (auto [x, y]: unvisited)
+        {
+            if (distance[x][y] < min_distance)
+            {
+                min_distance = distance[x][y]; 
+                current = Pos{x, y};
+            }
+        }
+    }
+
+    return 0xFFFF'FFFF;
 }
 
 
-template <typename T, typename U>
-auto part2(T points, U folds)
+template <typename T>
+auto part2(T input)
 {
     aoc::timer timer;
     return 0;
@@ -21,14 +96,19 @@ void run(const char* filename)
 {
     aoc::timer timer;
 
-    auto points = aoc::read_lines<int, int>(filename, R"((\d+),(\d+))");
-    auto folds  = aoc::read_lines<char, int>(filename, R"(fold\salong\s(\w)=(\d+))");
+    auto lines = aoc::read_lines(filename);
+    vector<vector<uint32_t>> input;
+    for (auto line: lines)
+    {
+        auto row = aoc::comprehend(line, [](auto c) -> uint32_t { return c - '0'; });  
+        input.push_back(row);
+    }
 
-    auto p1 = part1(points, folds);
+    auto p1 = part1(input);
     cout << "Part1: " << p1 << '\n';
     //aoc::check_result(p1, 0);
 
-    auto p2 = part2(points, folds);
+    auto p2 = part2(input);
     cout << "Part2: is read off the screen\n";
     //aoc::check_result(p2, 0);
 }
