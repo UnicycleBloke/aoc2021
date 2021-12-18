1ff58127467095da5fa3e1df298ba11e8932e1e0
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-18 11:41:58 +0000

    Solution for Day 18. One of those where the spec was super complicated and I needed a *lot* of tests.

--- day18/day18.cpp
+++ day18/day18.cpp
@@ -1,11 +1,242 @@
 #include "utils.h"
 
 
+struct Number
+{
+    int value{};
+    vector<Number> subs{}; 
+    
+    bool isdigit() const
+    {
+        return subs.size() == 0;  
+    }
+
+    void print(ostream& os, bool end)
+    {
+        if (isdigit()) 
+        {
+            os << value;
+        }
+        else
+        {
+            os << '[';        
+            subs[0].print(os, false);
+            os << ',';
+            subs[1].print(os, false);
+            os << ']';        
+        }
+
+        if (end) os << '\n';
+    }
+
+    void print()
+    {
+        print(cout, true);
+    }
+
+    string repr()
+    {
+        ostringstream os;
+        print(os, false);
+        return os.str();
+    }
+
+    int magnitude()
+    {
+        int result = 0;
+        if (isdigit()) 
+        {
+            result += value;
+        }
+        else
+        {
+            result += subs[0].magnitude() * 3;
+            result += subs[1].magnitude() * 2;
+        }
+        return result;
+    }
+};
+
+
+struct Splitter
+{
+    bool split(Number& number)
+    {
+        if (was_split) return was_split;
+
+        if (!number.isdigit()) 
+        {
+            split(number.subs[0]);
+            split(number.subs[1]);
+        }
+        else
+        {
+            if (number.value >= 10)
+            {
+                number.subs.push_back({number.value /2});
+                number.subs.push_back({(number.value + 1) /2});
+                number.value = 0;
+                was_split = true;
+            }
+        }
+
+        return was_split;
+    }
+
+    bool was_split{};
+};
+
+
+// Working out how to do this step was by far the hardest part of the problem. Some 
+// three hours of head-scratching, hacking and debugging.
+struct Exploder
+{
+    void explode(Number& number, int level)
+    {
+        if (!number.isdigit() && (level == 4) && !exploded)
+        {
+            left_value  = number.subs[0].value;
+            right_value = number.subs[1].value;
+
+            number.value = 0;
+            number.subs.clear();
+
+            exploded = true;
+            return;
+        }
+
+        if (!number.isdigit()) 
+        {
+            explode(number.subs[0], level + 1);
+            explode(number.subs[1], level + 1);
+        }
+        else
+        {
+            if (exploded)
+            {
+                if (!right_num) right_num = &number;
+            }
+            else
+            {
+                left_num = &number;
+            }
+        }
+    }
+
+    bool explode(Number& number)
+    {
+        explode(number, 0);
+        if (left_num)  left_num->value  += left_value;
+        if (right_num) right_num->value += right_value;
+        return exploded;
+    }
+
+    Number* left_num{};
+    Number* right_num{};
+    int     left_value{};
+    int     right_value{};
+    bool    exploded{};
+};
+
+
+
+Number parse(const string& line, int& pos)
+{
+    Number res;
+
+    char c = line[pos++];
+    if (c == '[')
+    {
+        res.subs.push_back(parse(line, pos));
+        ++pos; // Comma     
+        res.subs.push_back(parse(line, pos));
+        ++pos; // ]    
+    }
+    else
+    {
+        res.value = c - '0';
+    }
+
+    return res;
+}
+
+
+Number parse(const string& line)
+{
+    int pos = 0;
+    return parse(line, pos);
+}
+
+
+void reduce(Number& number)
+{
+    bool stop = false;
+    while (!stop)
+    {
+        stop = true;
+
+        Exploder e;
+        if (e.explode(number))
+        {
+            //cout << "explode: ";
+            //number.print();
+            stop = false;
+            continue;
+        }
+
+        Splitter s;
+        if (s.split(number))
+        {
+            //cout << "split:   ";
+            //number.print();
+            stop = false;
+            continue;
+        }
+    }
+}
+
+
+Number add(Number left, Number right)
+{
+    // cout << "   ";
+    // left.print();
+    // cout << " + ";
+    // right.print();
+
+    Number res{0};
+    res.subs.push_back(left);
+    res.subs.push_back(right);
+    // cout << " # ";
+    // res.print();
+
+    reduce(res);
+    // cout << " = ";
+    // res.print();
+
+    return res;
+}
+
+
 template <typename T>
 auto part1(T input)
 { 
     aoc::timer timer;
-    return 0;
+
+    Number left = parse(input[0]);
+    left.print();
+    cout << left.magnitude() << '\n';
+
+    for (auto i: aoc::range(1U, input.size()))
+    {
+        Number right = parse(input[i]);
+        right.print();
+        cout << right.magnitude() << '\n';
+        left = add(left, right);
+    }
+
+    left.print();  
+
+    return left.magnitude();
 }
 
 
@@ -13,21 +244,113 @@ template <typename T>
 auto part2(T input)
 {
     aoc::timer timer;
-    return 0;
+
+    int mag = 0;
+    for (auto i: aoc::range(input.size()))
+    {
+        for (auto j: aoc::range(i + 1, input.size()))
+        {
+            Number left  = parse(input[i]);
+            Number right = parse(input[j]);
+
+            left = add(left, right);
+            mag = max(mag, left.magnitude());
+        }
+
+        for (auto j: aoc::range(i + 1, input.size()))
+        {
+            Number left  = parse(input[i]);
+            Number right = parse(input[j]);
+
+            left = add(right, left);
+            mag = max(mag, left.magnitude());
+        }
+    }
+
+    return mag;
+}
+
+
+void test_magnitde(string str, int exp)
+{
+    auto num = parse(str);
+    //num.print();
+    aoc::check_result(num.magnitude(), exp);
+}
+
+
+void test_explode(string str, string exp)
+{
+    auto num = parse(str);
+    //num.print();
+
+    Exploder e;
+    e.explode(num);
+    //num.print();
+
+    if (num.repr() != exp)
+        std::cout << "ERROR: expected = " << exp << " but got = " << num.repr() << '\n';
 }
 
 
 void run(const char* filename)
 {
-    auto input = aoc::read_lines<string, string>(filename, R"((\w+)-(\w+))");
+    auto input = aoc::read_lines(filename);
+
+    //test_magnitde("[[1,2],[[3,4],5]]", 143);
+    //test_magnitde("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384);
+    //test_magnitde("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445);
+    //test_magnitde("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791);
+    //test_magnitde("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137);
+    //test_magnitde("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488);
+
+    //test_explode("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]");
+    //test_explode("[7,[6,[5,[4,[3,2]]]]]" ,"[7,[6,[5,[7,0]]]]");
+    //test_explode("[[6,[5,[4,[3,2]]]],1]" ,"[[6,[5,[7,0]]],3]");
+    //test_explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" ,"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]");
+    //test_explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]" ,"[[3,[2,[8,0]]],[9,[5,[7,0]]]]");
+
+    //auto left  = parse("[[[[4,3],4],4],[7,[[8,4],9]]]");
+    //auto right = parse("[1,1]");
+    //auto sum = add(left, right);
+    //string exp{"[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"};
+    //if (sum.repr() != exp)
+    //    std::cout << "ERROR: expected = " << exp << " but got = " << sum.repr() << '\n';
+
+    // vector<string> test1 = {
+    //     "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+    //     "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+    //     "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+    //     "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+    //     "[7,[5,[[3,8],[1,4]]]]",
+    //     "[[2,[2,2]],[8,[8,1]]]",
+    //     "[2,9]",
+    //     "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+    //     "[[[5,[7,4]],7],1]",
+    //     "[[[[4,2],2],6],[8,7]]" };
+    // auto res1 = part1(test1);
+
+    // vector<string> test2 = {
+    //     "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+    //     "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+    //     "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+    //     "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+    //     "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+    //     "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+    //     "[[[[5,4],[7,7]],8],[[8,3],8]]",
+    //     "[[9,3],[[9,9],[6,[4,9]]]]",
+    //     "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+    //     "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]" };
+    // auto res2 = part1(test2);
+    // aoc::check_result(res2, 4140);
 
     auto p1 = part1(input);
     cout << "Part1: " << p1 << '\n';
-    //aoc::check_result(p1, 0);
+    aoc::check_result(p1, 3647);
 
     auto p2 = part2(input);
     cout << "Part2: " << p2 << '\n';
-    //aoc::check_result(p2, 0);
+    aoc::check_result(p2, 4600);
 }
 
 
