9a554b6bb8fe199b1be2899f8cc6b561492348f6
Alan Chambers <unicycle.bloke@gmail.com>
2021-12-24 05:51:21 +0000

    Runner seems to be working for Day 24

--- day24/day24.cpp
+++ day24/day24.cpp
@@ -1,10 +1,99 @@
 #include "utils.h"
 
+enum class Op { inp, add, mul, div, mod, eql };
+
+map<string, Op> ops =
+{
+    { "inp", Op::inp},
+    { "add", Op::add},
+    { "mul", Op::mul},
+    { "div", Op::div},
+    { "mod", Op::mod},
+    { "eql", Op::eql}
+};
+
+
+int64_t read_input()
+{
+    return 10;
+}
+
+
+template <typename T>
+bool execute(T input)
+{
+    int64_t regs[4]{};
+
+    for (auto i: input)
+    {
+        auto instr = aoc::split(i, " ", false);
+ 
+        Op      op   = ops[instr[0]];
+        int     reg1 = instr[1][0] - 'w';
+        int     reg2 = -1;
+        int64_t imm  = 0;
+        if (instr.size() > 2)
+        {
+            if (isdigit(instr[2][0]) || instr[2][0] == '-')
+                imm = stoi(instr[2]);
+            else
+                reg2 = instr[2][0] - 'w';
+        }
+
+        int64_t val2 = (reg2 == -1) ? imm : regs[reg2]; 
+
+        switch (op)
+        {
+            case Op::inp:
+                regs[reg1] = read_input();
+                break;
+
+            case Op::add:
+                regs[reg1] += val2; 
+                break;
+                
+            case Op::mul:
+                regs[reg1] *= val2; 
+                break;
+
+            case Op::div:
+            {
+                if (val2 == 0) return false;
+
+                auto sign = aoc::sgn(regs[reg1]); 
+                val2 = (abs(regs[reg1]) / val2) * sign;
+                cout << val2 << '\n';
+                regs[reg1] = val2; 
+                break;
+            }
+
+            case Op::mod:
+                if (regs[reg1] < 0) return false;
+                if (val2 <= 0) return false;
+
+                regs[reg1] = regs[reg1] % val2; 
+                break;
+
+            case Op::eql:       
+                regs[reg1] = (regs[reg1] == val2);
+                break; 
+        }
+
+        for (auto r: aoc::range(4))
+            cout << char('w'+r) << "="<< regs[r] << " ";
+        cout << "\n"; 
+    }
+
+    cout << "complete" << '\n';
+    return true;
+}
+
 
 template <typename T>
 auto part1(T input)
 {
     aoc::timer timer;
+    execute(input);
     return 0;
 }
 
@@ -21,7 +110,7 @@ void run(const char* filename)
 {
     aoc::timer timer;
 
-    auto input = aoc::read_lines<uint64_t, uint64_t>(filename, R"((\d+),(\d+))");
+    auto input = aoc::read_lines(filename);
 
     auto p1 = part1(input);
     cout << "Part1: " << p1 << '\n';
